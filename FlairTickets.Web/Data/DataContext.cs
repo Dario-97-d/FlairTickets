db4d0eef2d95818ae87c3bc499211295d8ea056a
Dario Dias <dariodias97@gmail.com>
2023-06-26 09:56:27 +0100

    Add DataContext.cs Class DataContext, derived from IdentityDbContext, for later User Authentication and database usage

---
+++ FlairTickets.Web/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlairTickets.Web.Data
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
