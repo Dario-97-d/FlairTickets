90f2d5ae76f46cd97d3527b5eb0fff8db9c9c0bc
Dario Dias <dariodias97@gmail.com>
2023-07-10 22:49:43 +0100

    Add CRUD Airplanes and Airports

---
+++ FlairTickets.Web/Controllers/AirplanesController.cs
@@ -0,0 +1,126 @@
+﻿using System.Threading.Tasks;
+using FlairTickets.Web.Data.Entities;
+using FlairTickets.Web.Data.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlairTickets.Web.Controllers
+{
+    [Authorize]
+    public class AirplanesController : Controller
+    {
+        private readonly IAirplaneRepository _airplaneRepository;
+
+        public AirplanesController(IAirplaneRepository airplaneRepository)
+        {
+            _airplaneRepository = airplaneRepository;
+        }
+
+
+        // GET: Airplanes
+        public IActionResult Index()
+        {
+            return View(_airplaneRepository.GetAll());
+        }
+
+
+        // GET: Airplanes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var airplane = await _airplaneRepository.GetByIdAsync(id.Value);
+            if (airplane == null) return NotFound();
+
+            return View(airplane);
+        }
+
+
+        // GET: Airplanes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Airplanes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Model,Name,Seats")] Airplane airplane)
+        {
+            if (ModelState.IsValid)
+            {
+                await _airplaneRepository.CreateAsync(airplane);
+                return RedirectToAction(nameof(Index));
+            }
+
+            ModelState.AddModelError(string.Empty, $"Could not create {nameof(Airplane)}.");
+            return View(airplane);
+        }
+
+
+        // GET: Airplanes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var airplane = await _airplaneRepository.GetByIdAsync(id.Value);
+            if (airplane == null) return NotFound();
+
+            return View(airplane);
+        }
+
+        // POST: Airplanes/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Model,Name,Seats")] Airplane airplane)
+        {
+            if (id != airplane.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _airplaneRepository.UpdateAsync(airplane);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!await _airplaneRepository.ExistsAsync(airplane.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            ModelState.AddModelError(string.Empty, $"Could not update {nameof(Airplane)}.");
+            return View(airplane);
+        }
+
+
+        // GET: Airplanes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var airplane = await _airplaneRepository.GetByIdAsync(id.Value);
+            if (airplane == null) return NotFound();
+
+            return View(airplane);
+        }
+
+        // POST: Airplanes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _airplaneRepository.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ FlairTickets.Web/Controllers/AirportsController.cs
@@ -0,0 +1,126 @@
+﻿using System.Threading.Tasks;
+using FlairTickets.Web.Data.Entities;
+using FlairTickets.Web.Data.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlairTickets.Web.Controllers
+{
+    [Authorize]
+    public class AirportsController : Controller
+    {
+        private readonly IAirportRepository _airportRepository;
+
+        public AirportsController(IAirportRepository airportRepository)
+        {
+            _airportRepository = airportRepository;
+        }
+
+
+        // GET: Airports
+        public IActionResult Index()
+        {
+            return View(_airportRepository.GetAll());
+        }
+
+
+        // GET: Airports/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var airport = await _airportRepository.GetByIdAsync(id.Value);
+            if (airport == null) return NotFound();
+
+            return View(airport);
+        }
+
+
+        // GET: Airports/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Airports/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,IataCode,Name,City,Country")] Airport airport)
+        {
+            if (ModelState.IsValid)
+            {
+                await _airportRepository.CreateAsync(airport);
+                return RedirectToAction(nameof(Index));
+            }
+
+            ModelState.AddModelError(string.Empty, $"Could not create {nameof(Airport)}.");
+            return View(airport);
+        }
+
+
+        // GET: Airports/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var airport = await _airportRepository.GetByIdAsync(id.Value);
+            if (airport == null) return NotFound();
+
+            return View(airport);
+        }
+
+        // POST: Airports/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,IataCode,Name,City,Country")] Airport airport)
+        {
+            if (id != airport.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _airportRepository.UpdateAsync(airport);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!await _airportRepository.ExistsAsync(airport.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            ModelState.AddModelError(string.Empty, $"Could not update {nameof(Airport)}.");
+            return View(airport);
+        }
+
+
+        // GET: Airports/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var airport = await _airportRepository.GetByIdAsync(id.Value);
+            if (airport == null) return NotFound();
+
+            return View(airport);
+        }
+
+        // POST: Airports/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _airportRepository.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
